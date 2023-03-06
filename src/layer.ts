581fd30382acae0be6b583d23de102cd38f79aca
Ruslan Orazliev <rorazliev@outlook.com>
2023-03-05 23:57:02 -0500

    Define neural network constructor

--- src/layer.ts
+++ src/layer.ts
@@ -1,6 +1,30 @@
+import Neuron from './neuron';
 /**
  *
  */
-class Layer {}
+class Layer {
+	protected neurons: Array<Neuron> = [];
+	/**
+	 * @param {number} size
+	 */
+	constructor(size: number) {
+		for (let i = 0; i < size; i++) {
+			this.neurons.push(new Neuron());
+		}
+	}
+	/**
+	 * @param {number} index
+	 * @return {Neuron}
+	 */
+	getNeuron(index: number): Neuron {
+		return this.neurons[index];
+	}
+	/**
+	 * @return {number}
+	 */
+	getSize(): number {
+		return this.neurons.length;
+	}
+}
 
 export default Layer;
--- src/neural-network.ts
+++ src/neural-network.ts
@@ -1,6 +1,78 @@
+import Connection from './connection';
+import Layer from './layer';
 /**
  *
  */
-class NeuralNetwork {}
+type OptionsType = {
+	hiddenLayers: Array<number>;
+	inputSize: number;
+	outputSize: number;
+};
+/**
+ *
+ */
+const defaultOptions: OptionsType = {
+	hiddenLayers: [],
+	inputSize: 0,
+	outputSize: 0,
+};
+/**
+ *
+ */
+class NeuralNetwork {
+	/**
+	 *
+	 */
+	protected layers: Array<Layer> = [];
+	/**
+	 *
+	 */
+	protected options: OptionsType;
+	/**
+	 *
+	 */
+	protected sizes: Array<number> = [];
+	/**
+	 * @param {Partial<OptionsType>} options
+	 */
+	constructor(options: Partial<OptionsType> = {}) {
+		this.options = { ...defaultOptions, ...options };
+
+		const { inputSize, hiddenLayers, outputSize } = this.options;
+
+		if (inputSize && outputSize) {
+			this.sizes = [inputSize].concat(hiddenLayers, [outputSize]);
+		}
+	}
+	/**
+	 *
+	 */
+	protected initialize(): void {
+		const sizes = this.sizes;
+
+		if (sizes.length === 0) {
+			throw new Error('Please, set up sizes for layers');
+		}
+
+		const layers: Layer[] = [];
+		let previous = new Layer(sizes[0]);
+		layers.push(previous);
+
+		for (let i = 1; i < sizes.length; i++) {
+			const current = new Layer(sizes[i]);
+
+			for (let l = 0; l < previous.getSize(); l++) {
+				for (let k = 0; k < current.getSize(); k++) {
+					new Connection(previous.getNeuron(l), current.getNeuron(k));
+				}
+			}
+
+			layers.push(current);
+			previous = current;
+		}
+
+		this.layers = layers;
+	}
+}
 
 export default NeuralNetwork;
